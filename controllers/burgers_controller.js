427267980523ca4471b8acb4260bf90a0c9fbf68
Tito Rivera <titorivera0707@gmail.com>
2020-12-18 17:58:11 -0800

    Updating files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const burger = require("../models/burger");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.updateOne(
+      {
+        sleepy: req.body.sleepy
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
+
+module.exports = router
