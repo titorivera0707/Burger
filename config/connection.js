63a40244dc234d1af6ffd66c94f14833eb2880db
Tito Rivera <titorivera0707@gmail.com>
2021-01-03 10:43:01 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -13,13 +13,13 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
