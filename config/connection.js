84b0cef611f1f5008dd9d2967857edd1a21089e1
Tito Rivera <titorivera0707@gmail.com>
2021-01-03 08:43:30 -0800

    trouble

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
+// var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -13,13 +13,13 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "password",
-//   database: "burgers_db"
-// });
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
 
 // Make connection.
 connection.connect(function(err) {
@@ -31,5 +31,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
-connection.connect()
+// connection.connect()
 module.exports = connection;
